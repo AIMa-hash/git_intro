16e083179ccdcdbf2e7ad2495ab790bf4dbd7119
Manish <manishforphysics@gmail.com>
2021-04-19 14:48:45 +0530

    modified git_intro.py in branch manish

--- git_intro.py
+++ git_intro.py
@@ -10,3 +10,4 @@ else:
 #git commit -a <file>
 #use "git restore --staged <file>..." to unstages
 print("not sure why it did not change")
+print("not sure if this second git branch is going to be shown in the git log or not. let's see.")
