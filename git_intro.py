617c3cbf4d03ce243b5c132a5b11d56380e2d858
Manish <manishforphysics@gmail.com>
2021-04-19 15:00:53 +0530

    uesful info

--- git_intro.py
+++ git_intro.py
@@ -12,3 +12,5 @@ else:
 print("not sure why it did not change")
 print("not sure if this second git branch is going to be shown in the git log or not. let's see.")
 print("last statement")
+"""Previous HEAD position was 16e0831 modified git_intro.py in branch manish
+Switched to branch 'manish'"""
